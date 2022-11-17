37f10d6c2a36ecb2131ad43ceac5a9ad5d2379a5
feinke <feinke@gmail.com>
2022-11-17 18:19:00 +0800

    add demo workflow

--- api/app.py
+++ api/app.py
@@ -1,16 +1,12 @@
-# from flask import Flask, jsonify
-# from pytube import YouTube
+import os
 
-# app = Flask(__name__)
+def main():
+    who = os.environ["INPUT_MYINPUT"]
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, Docker!'
+    my_output = f"Hello {who}"
 
-# @app.route('/<id>')
-# def getTheId(id):
-#     yt = YouTube(f'https://youtu.be/{id}')
-#     print(yt.streams.get_audio_only())
-#     return 'ok1'
-    # return yt.streams.filter(only_audio=True).first().download()
-    # return f'Hello, tsssest {id}!'
+    print(f"::set-output name=myOutput::{my_output}")
+
+
+if __name__ == "__main__":
+    main()
